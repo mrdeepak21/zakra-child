3f8093959780157c73e2911aec597de80cceb63d
Reloaded <deepak@designandcode.tech>
2023-12-29 21:38:49 +0530

    test

--- protected.js
+++ protected.js
@@ -3,7 +3,7 @@ const page = slug[slug.length-2];
 const _password = 'cobra1@#';
 const already_loggedin = Boolean(getCookie('_protected_loggedIn'));
 
-if(!already_loggedin && page!=='contact-us-1' && page!=='') {
+if(!already_loggedin && page!=='contact-us-1' && page!=='' && page!=='thank-you') {
     if(_password !== prompt('Please enter password')){
         window.location.reload();
     } else {
